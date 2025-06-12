64f3c8eb6b67e376675f568058c0562d6cd70dfd
AKSHIT <akshitgoyal545@gmail.com>
2025-06-12 10:48:49 +0530

    soluition1

---
+++ solution1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class solution1{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the birth date");
+        long t = sc.nextInt();
+        Long r = 2024- t;
+        System.out.println("Harry age in 2024 is "+ r);
+        
+    }
+}
