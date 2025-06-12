3a1725deda634842ad9d14d54b3f54d021b1d599
AKSHIT <akshitgoyal545@gmail.com>
2025-06-12 10:53:38 +0530

    solution2

---
+++ solution2.java
@@ -0,0 +1,7 @@
+class solution2{
+    public static void main(String[] args) {
+        float t = (float) (10.8*1.6);
+        System.out.println("the distance " + 10.8 +" km in miles is "+t);
+
+    }
+}
